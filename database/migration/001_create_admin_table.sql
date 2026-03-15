48610cc874175b73bd82c7bfb43ef18f40b773a0
Francis Ge <francisge527@gmail.com>
2026-03-15 21:18:36 +0800

    add migrations

---
+++ database/migration/001_create_admin_table.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE centavo_changer_monitoring;
+
+CREATE TABLE admin (
+	admin_id SERIAL PRIMARY KEY,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	hashed_password TEXT NOT NULL,
+	account_name VARCHAR(25) NOT NULL
+);
---
+++ database/migration/002_create_admin_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE storage (
+	storage_id SERIAL PRIMARY KEY,
+	one DECIMAL(10, 2),
+	five DECIMAL(10, 2),
+	ten DECIMAL(10, 2),
+	twenty DECIMAL(10, 2)
+);

b7c3bf5c34a13ab1b3e7499f1c874368ca56a0ca
Francis Ge <francisge527@gmail.com>
2026-03-24 21:05:01 +0800

    add transactions routes to index.js file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import transactionsRoutes from "./routes/transactions.route.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.use("/api", transactionsRoutes);
 
 // default check
 app.use("/", (req, res) => {

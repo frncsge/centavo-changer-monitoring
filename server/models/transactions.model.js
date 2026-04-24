7ff99d4a946e621b73c08365378b69167c066bad
Francis Ge <francisge527@gmail.com>
2026-04-24 21:57:43 +0800

    fix: include peso_dispensed table in query using join

--- server/models/transactions.model.js
+++ server/models/transactions.model.js
@@ -2,7 +2,14 @@ import pool from "../../config/dbConfig.js";
 
 export const fetchTransactions = async () => {
   try {
-    const result = await pool.query("SELECT * FROM transactions");
+    const result = await pool.query(`
+        SELECT 
+          txn.*,
+	        pd.peso_value,
+	        pd.quantity
+        FROM transactions txn
+        JOIN peso_dispensed pd ON txn.transaction_id = pd.transaction_id;
+      `);
     return result.rows;
   } catch (error) {
     console.error(

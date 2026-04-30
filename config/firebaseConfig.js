8a4a467dc9bdfe1ab4e996c686333ee413ad1245
Francis Ge <francisge527@gmail.com>
2026-04-30 22:25:05 +0800

    chore: add config for firebase auth

---
+++ config/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCAK2VXxz31JKEC3Ls-2L6rahSMtUXn4ds",
+  authDomain: "centavo-changer-monitoring.firebaseapp.com",
+  projectId: "centavo-changer-monitoring",
+  storageBucket: "centavo-changer-monitoring.firebasestorage.app",
+  messagingSenderId: "647347749260",
+  appId: "1:647347749260:web:cf4fcce885b32885e416a9",
+  measurementId: "G-R7WE6X2QB8",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);

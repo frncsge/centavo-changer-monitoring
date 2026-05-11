60960c573f37397dce13789efc6ff25559322091
Francis Ge <francisge527@gmail.com>
2026-05-11 13:41:37 +0800

    feat: add route for creating transactions

--- server/routes/transactions.route.js
+++ server/routes/transactions.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getTransactions } from "../controllers/transactions.controller.js";
+import { getTransactions, createTransaction } from "../controllers/transactions.controller.js";
 import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.get("/transactions", authenticateUser, getTransactions);
+router.post("/transactions", createTransaction);
 
 export default router;

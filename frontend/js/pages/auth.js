3f46711a48002f1635e73bc5a738a1ec7c4be4be
Francis Ge <francisge527@gmail.com>
2026-05-12 13:13:23 +0800

    fix: supabaseClient.js file import

--- frontend/js/pages/auth.js
+++ frontend/js/pages/auth.js
@@ -1,4 +1,4 @@
-import supabase from "../../../config/supabaseConfig.js";
+import supabase from "../../../config/supabaseClient.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   document

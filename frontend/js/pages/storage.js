3923870a5e4c45106490cd500972aa0be2ba65da
Francis Ge <francisge527@gmail.com>
2026-05-27 17:48:02 +0800

    feat: fetch storage from server

--- frontend/js/pages/storage.js
+++ frontend/js/pages/storage.js
@@ -1,10 +1,30 @@
 import { checkUserAuth } from "../auth/session.js";
+import { authFetch } from "../api/authFetch.js";
 
 window.addEventListener("DOMContentLoaded", async () => {
-  // check if user is logged in
-  const isLoggedIn = await checkUserAuth();
+  try {
+    const res = await authFetch("/machine-storage");
 
-  if (!isLoggedIn) {
-    window.location.replace("http://127.0.0.1:5501/frontend/html/login.html");
+    // if user is unauthenticated
+    if (res.status === 401) {
+      window.location.replace("http://127.0.0.1:5501/frontend/html/login.html");
+      return;
+    }
+
+    if (!res.ok) {
+      console.error(
+        "Machine storage fetch failed:",
+        res.status,
+        res.statusText,
+      );
+      return;
+    }
+
+    const data = await res.json();
+    const { storage } = data;
+
+    console.log("Storage: ", storage);
+  } catch (error) {
+    console.error("Error loading transactions:", err);
   }
 });

41964f4ffa5d8e3559e713b43841906ce8e921ce
Francis Ge <francisge527@gmail.com>
2026-05-31 21:38:03 +0800

    feat: add funtcion for storing machine storage to the database

--- server/models/machineStorage.model.js
+++ server/models/machineStorage.model.js
@@ -24,3 +24,42 @@ export const fetchMachineStorage = async (userId) => {
     throw error;
   }
 };
+
+export const storeRefill = async ({ machineId, pesoValue, quantity }) => {
+  const client = await pool.connect();
+
+  try {
+    await client.query("BEGIN");
+
+    const result = await client.query(
+      `
+        INSERT INTO refills (machine_id)
+        VALUES ($1)
+        RETURNING refill_id;
+      `,
+      [machineId],
+    );
+
+    const refillId = result.rows[0].refill_id;
+
+    await client.query(
+      `
+        INSERT INTO peso_refilled
+        VALUES ($1, $2, $3)
+      `,
+      [refillId, pesoValue, quantity],
+    );
+
+    await client.query("COMMIT");
+  } catch (error) {
+    if (client) await client.query("ROLLBACK");
+
+    console.error(
+      "An error occured while trying to create a machine storage refill record to the database:",
+      error,
+    );
+    throw error;
+  } finally {
+    client.release();
+  }
+};

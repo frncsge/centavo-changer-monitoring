9d174c3098e9ca7cdbf37b16e26e58e01e7a7d36
Francis Ge <francisge527@gmail.com>
2026-06-08 18:25:39 +0800

    fix: autheticate user for transaction routes

--- server/routes/transactions.route.js
+++ server/routes/transactions.route.js
@@ -7,7 +7,7 @@ import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
-router.get("/machines/:id/transactions", getTransactions);
-router.post("/machines/:id/transactions", createTransaction);
+router.get("/machines/:id/transactions", authenticateUser, getTransactions);
+router.post("/machines/:id/transactions", authenticateUser, createTransaction);
 
 export default router;

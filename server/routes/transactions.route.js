954643ab9137dd266f0b9b7767ac8092e4a0ddf1
Francis Ge <francisge527@gmail.com>
2026-06-08 18:30:00 +0800

    fix: remove authenticateUser middleware for post transactions route

--- server/routes/transactions.route.js
+++ server/routes/transactions.route.js
@@ -8,6 +8,6 @@ import { authenticateUser } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
 router.get("/machines/:id/transactions", authenticateUser, getTransactions);
-router.post("/machines/:id/transactions", authenticateUser, createTransaction);
+router.post("/machines/:id/transactions", createTransaction);
 
 export default router;
